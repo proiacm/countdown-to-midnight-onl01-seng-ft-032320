f24a04c1f842a7b0c0b7681b572234c8cbb3edbe
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 16:39:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+def countdown(number)
+  number = 10
+  
+while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+ # sleep
+end
 
-def countdown
+   "HAPPY NEW YEAR!"
+  
 end
